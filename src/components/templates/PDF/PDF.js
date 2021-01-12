28321c11a7e64a941f83c0f0c885a8ba85600285
Fabián Veliz <velizfabianhoracio@gmail.com>
2021-01-11 16:27:48 -0800

    Fix stylesheet path and mobile styles

--- src/components/templates/PDF/PDF.js
+++ src/components/templates/PDF/PDF.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 // TODO: Move to env vars
-const host = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://fabianveliz.com";
+const isDevelopment = process.env.NODE_ENV === "development";
+const host = isDevelopment ? "http://localhost:3000" : "https://fabianveliz.com";
 
 const PDF = ({ children }) => (
   <html style={{fontSize: '10px'}}>
     <head>
       <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-      <link rel="stylesheet" href={`${host}/_next/static/css/styles.chunk.css`} />
+      <link rel="stylesheet" href={`${host}/static/cv.css`} />
     </head>
     <body>
       {children}
